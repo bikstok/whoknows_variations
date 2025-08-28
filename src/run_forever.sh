4614572d9b94235e1f812c017fa6089d2f299035
Bikstok <jkjaerulff@gmail.com>
2025-08-28 20:20:53 +0200

    fixed run_forever script to modern using shellfix

--- src/run_forever.sh
+++ src/run_forever.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-PYTHON_SCRIPT_PATH=$1
-
-TMP="This variable might become useful at some point. Otherwise delete it." 
+PYTHON_SCRIPT_PATH="$1"
 
 while true
 do
-    python2 $PYTHON_SCRIPT_PATH
-    if [ $? -ne 0 ]; then
-        echo "Script crashed with exit code $?. Restarting..." >&2
+    if ! python3 "$PYTHON_SCRIPT_PATH"; then
+        echo "Script crashed. Restarting..." >&2
         sleep 1
     fi
 done
